eb221b627bcf83f7e89ad75355ebb846eb28ea1c
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-06-13 14:15:47 +0200

    Logs the first name, last name and email when user presses on 'Sign Up'

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ const request = require("request");
 const app = express();
 const port = 3000;
 
+// Using this so we can refer to static files
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Adds signup.html to home page
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html")
+});
+
+app.post("/", function(req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
 });
 
 // app constant is used to listen to port 3000
