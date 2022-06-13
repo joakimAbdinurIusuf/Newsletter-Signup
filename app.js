f8a5fa7224f1cadc9433acdc5a8c7c43d4994260
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-06-13 14:00:04 +0200

    File is static so not showing properly on localhost:3000.

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const request = require("request");
 const app = express();
 const port = 3000;
 
+// Adds signup.html to home page
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html")
+});
+
 // app constant is used to listen to port 3000
 app.listen(port, function() {
   console.log("Server is running on port " + port);
