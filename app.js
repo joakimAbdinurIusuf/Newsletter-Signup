b7e7d12399b3295f4ada97b8c4923fa930bdecdd
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-06-13 13:14:14 +0200

    Adds bootstrap.

--- app.js
+++ app.js
@@ -3,3 +3,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+
+const app = express();
+const port = 3000;
+
+// app constant is used to listen to port 3000
+app.listen(port, function() {
+  console.log("Server is running on port " + port);
+});
